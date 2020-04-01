e508865d7a159c03440e9bc5ca573c16c9b9a170
shedinho20 <shedinhoshedrach@gmail.com>
2020-04-01 20:02:51 +0300

    updated mobile ver

--- src/end/main.js
+++ src/end/main.js
@@ -1,11 +1,13 @@
 const menu = document.getElementById("menu");
 const menuContent = document.getElementById("menu-content");
 const nav = document.getElementById("nav");
-
+const li = document.getElementById("click");
 menu.addEventListener("click", e => {
     setTimeout(() => {
         nav.classList.toggle("open");
-    }, 0.3);
+    }, 0.1);
     menu.classList.toggle("open");
     menuContent.classList.toggle("open");
 });
+
+li.addEventListener()
